b00a266783ead405e9840587938ec3a01086a3eb
ilyass <64425437+Cosinusjustafonction@users.noreply.github.com>
2021-09-13 21:36:11 +0100

    USACO mixing milk
    Farming is competitive business -- particularly milk production. Farmer John figures that if he doesn't innovate in his milk production methods, his dairy business could get creamed!
    Fortunately, Farmer John has a good idea. His three prize dairy cows Bessie, Elsie, and Mildred each produce milk with a slightly different taste, and he plans to mix these together to get the perfect blend of flavors.
    
    To mix the three different milks, he takes three buckets containing milk from the three cows. The buckets may have different sizes, and may not be completely full. He then pours bucket 1 into bucket 2, then bucket 2 into bucket 3, then bucket 3 into bucket 1, then bucket 1 into bucket 2, and so on in a cyclic fashion, for a total of 100 pour operations (so the 100th pour would be from bucket 1 into bucket 2). When Farmer John pours from bucket a into bucket b, he pours as much milk as possible until either bucket a becomes empty or bucket b becomes full.
    
    Please tell Farmer John how much milk will be in each bucket after he finishes all 100 pours.

---
+++ Usaco_mixing_milk.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct bucket{
+    int capacity , milk ;
+
+};
+void permutation(bucket &a , bucket &b) {
+    vector<int> t ;
+    if (a.milk + b.milk > b.capacity){
+        a.milk  = abs(b.capacity - (a.milk+b.milk));
+        b.milk = b.capacity;
+    }
+    else {
+        b.milk = a.milk + b.milk;
+        a.milk = 0;
+    }
+   
+}
+
+int main(){
+	vector<int> t ;
+	int pours ;
+	freopen(("mixmilk.in"),"r",stdin);
+    freopen(("mixmilk.out"),"w",stdout); 
+    bucket a,b,c;
+    cin >> a.capacity >> a.milk ;
+    cin >> b.capacity >> b.milk ;
+    cin >> c.capacity >> c.milk ;
+    while (pours < 100){
+        if (pours == 100){
+            break;
+        }
+        permutation(a,b);
+        pours +=1;
+        if (pours == 100){
+            break;
+        }
+        permutation(b,c);
+        pours +=1;
+        if (pours == 100){
+            break;
+        }
+        permutation(c,a);
+        pours +=1;
+    }
+    cout << a.milk << endl;
+    cout << b.milk << endl;
+    cout << c.milk << endl;
+
+
+}
