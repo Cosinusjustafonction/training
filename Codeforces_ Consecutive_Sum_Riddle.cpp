34e8c234dbdf0e4578c1014914c5565a42ab3c2c
ilyass <64425437+Cosinusjustafonction@users.noreply.github.com>
2021-10-08 21:21:35 +0100

    Consecutive Sum Riddle
    Theofanis has a riddle for you and if you manage to solve it, he will give you a Cypriot snack halloumi for free (Cypriot cheese).
    
    You are given an integer n. You need to find two integers l and r such that −1018≤l<r≤1018 and l+(l+1)+…+(r−1)+r=n.
    
    Input

---
+++ Codeforces_ Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+int main(){
+    ll n ,t; 
+    cin >> n ;
+    for (int i = 0 ; i < n ; i++){
+        cin >> t ;
+        cout << -(t-1) << " " << t << endl;
+    }
+      
+
+}
